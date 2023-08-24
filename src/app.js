cbcef8126473f487d0c8630f42358203817aeca8
lindamaduka <135467647+lindamaduka@users.noreply.github.com>
2023-08-24 17:12:37 +0100

    Updated the search engine

--- src/app.js
+++ src/app.js
@@ -37,12 +37,24 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute(
     "src",
-    "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png"
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "a5619d1cb307134b8f4771e2t2ca575o";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Enugu&key=a5619d1cb307134b8f4771e2t2ca575o&units=metric";
+function search(city) {
+  let apiKey = "a5619d1cb307134b8f4771e2t2ca575o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Enugu");
