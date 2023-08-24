1701a5a4d0eb6d1fdfcb530b9f4966229773d177
lindamaduka <135467647+lindamaduka@users.noreply.github.com>
2023-08-24 05:06:00 +0100

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,21 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data.temperature.current);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "a5619d1cb307134b8f4771e2t2ca575o";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Enugu&key=a5619d1cb307134b8f4771e2t2ca575o&units=metric";
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
