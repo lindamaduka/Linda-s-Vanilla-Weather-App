0bf084a82cf2ccbff6c80bf8ee46b1df63829ec2
lindamaduka <135467647+lindamaduka@users.noreply.github.com>
2023-08-24 05:27:27 +0100

    Changed the icon

--- src/app.js
+++ src/app.js
@@ -28,12 +28,17 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = response.data.wind.speed;
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png"
+  );
 }
 
 let apiKey = "a5619d1cb307134b8f4771e2t2ca575o";
